f2081739e16add5ac8a606dc5e2e40a07e29e3db
nishikubo0521 <nishikubo0521@gmail.com>
2018-01-02 12:32:43 +0900

    start
    set up a project

---
+++ test.js
@@ -0,0 +1,36 @@
+// WEVサーバー関連モジュール
+express = require('express');
+app = express();
+http = require('http').Server(app);
+io = require('socket.io')(http);
+
+
+// ブラウザからのリクエストに関係なく動くnodejs上の処理
+count = 0;
+setInterval(function(){
+	count ++;
+	console.log('試行回数: ' + count);
+}, 5000);
+
+
+// サーバーの起動
+console.log('稼働中...')
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+
+// ルーティング
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+// ソケット処理
+io.on('connection', function(socket){
+
+	// メッセージが来た時、そのメッセージをビューに返す。
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg + count );
+	});
+});
